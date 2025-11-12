798f723fdb25cb9b6a3cede8b058405c6b5c646e
Juan Veronez <juan.monteirov@gmail.com>
2025-11-12 16:15:31 -0300

    make 1st module exercises

---
+++ src/exercises1/Calculator.java
@@ -0,0 +1,8 @@
+package exercises1;
+
+// Crie uma classe Calculadora com um método que recebe um número como parâmetro e retorna o dobro desse número.
+public class Calculator {
+    double getValueDouble(double value) {
+        return value * 2;
+    }
+}
---
+++ src/exercises1/Person.java
@@ -0,0 +1,8 @@
+package exercises1;
+
+// Crie uma classe Pessoa com um método que exibe "Olá, mundo!" no console.
+public class Person {
+    void sayHi() {
+        System.out.println("Olá, tudo bem?");
+    }
+}
