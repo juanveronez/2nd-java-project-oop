1bf67ccf644fa6f81106815cc86e3566e55a61fd
Juan Veronez <juan.monteirov@gmail.com>
2025-11-13 13:23:40 -0300

    move exercises to new folder

--- src/com/screenmatch/exercises/module1/Calculator.java
+++ src/com/screenmatch/exercises/module1/Calculator.java
@@ -1,4 +1,4 @@
-package exercises1;
+package com.screenmatch.exercises.module1;
 
 // Crie uma classe Calculadora com um método que recebe um número como parâmetro e retorna o dobro desse número.
 public class Calculator {
--- src/com/screenmatch/exercises/module1/Person.java
+++ src/com/screenmatch/exercises/module1/Person.java
@@ -1,4 +1,4 @@
-package exercises1;
+package com.screenmatch.exercises.module1;
 
 // Crie uma classe Pessoa com um método que exibe "Olá, mundo!" no console.
 public class Person {
