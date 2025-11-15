016c372b997bcea4b8c918b7b0fdf5ac0c171cc0
Juan Veronez <juan.monteirov@gmail.com>
2025-11-15 10:40:33 -0300

    Create an interface to create a `Classifiable` contract

---
+++ src/com/screenmatch/models/Episode.java
@@ -0,0 +1,31 @@
+package com.screenmatch.models;
+
+public class Episode {
+    private int number;
+    private String name;
+    private Series series;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Series getSeries() {
+        return series;
+    }
+
+    public void setSeries(Series series) {
+        this.series = series;
+    }
+}
--- src/com/screenmatch/models/Movie.java
+++ src/com/screenmatch/models/Movie.java
@@ -1,6 +1,8 @@
 package com.screenmatch.models;
 
-public class Movie extends Title {
+import com.screenmatch.metrics.Classifiable;
+
+public class Movie extends Title implements Classifiable {
     private String director;
 
     public String getDirector() {
@@ -9,5 +11,10 @@ public class Movie extends Title {
 
     public void setDirector(String director) {
         this.director = director;
+    }
+
+    @Override
+    public int getClassification() {
+        return 0;
     }
 }
